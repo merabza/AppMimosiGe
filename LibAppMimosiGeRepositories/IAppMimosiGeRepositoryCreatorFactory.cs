2a165de2e8a595fee8a5dffd491cfcf24186c684
merabza <merabza@gmail.com>
2026-09-08 13:24:40 +0400

    202609081324

--- LibAppMimosiGeRepositories/IAppMimosiGeRepositoryCreatorFactory.cs
+++
@@ -1,8 +0,0 @@
-//Created by RepositoryCreatorFactoryInterfaceCreator at 2/15/2025 11:07:44 AM
-
-namespace LibAppMimosiGeRepositories;
-
-public interface IAppMimosiGeRepositoryCreatorFactory
-{
-    IAppMimosiGeRepository GetAppMimosiGeRepository();
-}
